8a673685184ecb5dee40f7e3cbd410003d8fe465
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2021-08-13 00:22:16 -0700

    Update remove_big_paradigms.py

--- predictions/remove_big_paradigms.py
+++ predictions/remove_big_paradigms.py
@@ -14,9 +14,9 @@ for index, line in enumerate(lines):
     if line == '\n':
         indexlist_id.append(index)
 
-with open("Language_remove_big.txt", "w") as f29: ##remove big paradigms(equal to or larger than 20 words) 
+with open("~/data/Language_remove_big.txt", "w") as f_new: ##remove big paradigms(equal to or larger than 20 words) 
     for i in range(1,len(indexlist_id)):
         if 1< indexlist_id[i]-indexlist_id[i-1]-1 <=20:
             for n in range(len(lines)):
                 if indexlist_id[i-1]<n<=indexlist_id[i]:
-                    f29.write(lines[n])
+                    f_new.write(lines[n])
