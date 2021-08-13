6f1460fd844cfd88427231b715d017259d9a9a63
Changbing Yang <58204931+changbingY@users.noreply.github.com>
2021-08-13 00:40:08 -0700

    Update remove_big_paradigms.py

--- predictions/remove_big_paradigms.py
+++ predictions/remove_big_paradigms.py
@@ -6,7 +6,7 @@ import numpy as np
 import copy
 import collections
 
-file = open("~/data/Language.5.preds",'r')
+file = open("../data/Language.5.preds",'r')
 lines = file.readlines()
 datalists = []
 indexlist_id = []
@@ -14,7 +14,7 @@ for index, line in enumerate(lines):
     if line == '\n':
         indexlist_id.append(index)
 
-with open("~/data/Language_remove_big.txt", "w") as f_new: ##remove big paradigms(equal to or larger than 20 words) 
+with open("../data/Language_remove_big.txt", "w") as f_new: ##remove big paradigms(equal to or larger than 20 words) 
     for i in range(1,len(indexlist_id)):
         if 1< indexlist_id[i]-indexlist_id[i-1]-1 <=20:
             for n in range(len(lines)):
